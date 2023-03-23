5e3393428e1f326c4b0da13550c78b174f946b68
diegoProFP <diego.di1@educa.madrid.org>
2023-03-24 00:11:54 +0100

    Subida inicial del proyecto

---
+++ Spotify/src/spotify/exception/OperacionNoPermitidaException.java
@@ -0,0 +1,10 @@
+package spotify.exception;
+
+public class OperacionNoPermitidaException extends Exception {
+
+	public OperacionNoPermitidaException(String mensaje) {
+		super(mensaje);
+	}
+	
+
+}
