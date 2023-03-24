4a4eed212b726e662289e13d0f821c923db3b0ca
schris53 <clucas@educa.madrid.org>
2023-03-24 10:10:37 +0100

    hecho cambios para generar el javadoc.

--- Spotify/src/spotify/clucas/Playlist.java
+++ Spotify/src/spotify/clucas/Playlist.java
@@ -1,9 +1,18 @@
 package spotify.clucas;
 
+
 import java.util.ArrayList;
+
 import java.util.List;
 
+
 import spotify.exception.DatosInvalidosException;
+
+/** Esta clase lleva los atrivutos del nombre del artista, un arrayList que contiene todas sus canciones y la duracion total en segundo de las canciones
+ * @author clucas@educa.madrid.org 
+ * @version 1.5
+ */
+
 
 //Poner comentario de la clase, con la descripción de qué es lo que hace  
 //De autor poned vuestro email de educaMadrid. 
@@ -14,13 +23,23 @@ public class Playlist {
 	private int totalSegundos;
 	private List<Cancion> canciones;
 
+	/**
+	 * @param este metodo obtenerDuracionTotal 
+	 * @return el total de los segundos pasados a minutos
+	 */
 	
 	// Poner descripcion, valor de retorno.
 	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
 	public double obtenerDuracionTotal() {
 		return (double) totalSegundos / 60;
 	}
-		
+	
+	/**@apiNote este metodo agrega canciones al ArrayList de las canciones el artista 
+	 * 
+	 * @param nueva es la cancion que se va a agregar a las playlist del artista 
+	 * @return devuelve exito si la cansion ha sido añadida 
+	 * @throws DatosInvalidosException
+	 */
 		
 		
 	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
@@ -44,7 +63,11 @@ public class Playlist {
 		return exito;
 	}
 
-
+	/**
+	 * @apiNote con esta clase , introducimos una palabra o parte de ella , dando igual si es mayusculas o minusculas y bsuca en el arrayList el nombre de la cancion, hasta encontrarla, siempre que haya canciones en el arraylist
+	 * @param nombre es el dato string que vamos a comparar con los distintos string que hay en el array
+	 * @return devuelve "encontrado" que es true o false dependiendo de si encuentra o no 
+	 */
 	
 	// Poner descripcion, parametros de entrada, valor de retorno, y excepción.
 	//Existe desde la version 1.5
@@ -65,6 +88,14 @@ public class Playlist {
 		return encontrado;
 	}
 	
+	
+	
+	/**@deprecated este metodo ha pasado a mejor vida, ahora se utiliza mas otro metodo {@link} encontrarCancionPorNombre(String nombre)
+	 * 
+	 * @param nombreCancion el dato nombreCancion verificaba si habia ese nombre de cancion 
+	 * @return encontrado , que podia ser true si lo encontraba o false si no daba con ello
+	 * 
+	 */
 	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
 	// encontrarCancionPorNombre(String nombre)
 	//Existe desde la version 1.0
