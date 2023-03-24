558967bda7f53e2456673e2f3961f4e3453b80b0
juanan30 <alumno@localhost>
2023-03-24 10:05:04 +0100

    cambio3 JAVADOC fuera del proyecto

--- Spotify/doc/package-search-index.js
+++ Spotify/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"spotify.juan.aparicio5"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"spotify.base"},{"l":"spotify.diegodi1"},{"l":"spotify.exception"},{"l":"spotify.juan.aparicio5"}];updateSearchResults();
