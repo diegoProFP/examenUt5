0fe4534886ce4f2cd54756b8d8d45d378de280fb
alexgmzdev <contacto.alejandrogimenez@gmail.com>
2023-03-24 10:04:41 +0100

    realizados primeros javadoc de la clase playlist

--- Spotify/src/spotify/alejandrogimenez2/Playlist.java
+++ Spotify/src/spotify/alejandrogimenez2/Playlist.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import spotify.exception.DatosInvalidosException;
 
-//Poner comentario de la clase, con la descripción de qué es lo que hace  
-//De autor poned vuestro email de educaMadrid. 
-//La version de la clase es la 1.5
+/**
+ * Clase que maneja todas las funciones de la playlist, con esta podremos obtener info de nuestra playlist, agregar y buscar
+ * canciones tan por nombre como buscar en general.
+ * @author alejandro.gimenez2@educa.madrid.org
+ * @version 1.5
+ */
 public class Playlist {
 
 	private String nombre;
@@ -15,17 +18,26 @@ public class Playlist {
 	private List<Cancion> canciones;
 
 	
-	// Poner descripcion, valor de retorno.
-	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
+	/**
+	 * Recibimos la duracion total que se obtiene con todas las canciones que han sido añadidas a nuestra playlist
+	 * Esto se obtiene ya que usamos el total de segundo de la cancion y lo dividimos entre 60 para obtener dicha duración
+	 * 
+	 * @return numero total de duracion de la playlist
+	 */
 	public double obtenerDuracionTotal() {
 		return (double) totalSegundos / 60;
 	}
 		
 		
-		
-	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
-	// referenciar tanto a las clases Cancion y DatosInvalidosException
-	//Existe desde la version 1.0
+	/**
+	 * Con esta función agregamos una cancion a nuestra playlist, comprobando si el nombre de cancion buscado existe
+	 * si esta existe se introduce a la playlist, si no, lanza un error.	
+	 * 
+	 * @param nueva refiriendose a la cancion que se añade 
+	 * @return devuelve si ha sido exitosa la accion de agragar una cancion a la playlist
+	 * @throws DatosInvalidosException
+	 * @since 1.0
+	 */
 	public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
 
 		boolean exito = false;
@@ -45,7 +57,13 @@ public class Playlist {
 	}
 
 
-	
+	/**
+	 * Funcion que busca el nombre de la cancion dentro de la playlist, recibiendo una respuesta de si es encontrada o no.
+	 * 
+	 * @param nombre
+	 * @return si encuentra dicha cancion o no
+	 * @since 1.5
+	 */
 	// Poner descripcion, parametros de entrada, valor de retorno, y excepción.
 	//Existe desde la version 1.5
 	public boolean encontrarCancionPorNombre(String nombre) {
@@ -65,6 +83,11 @@ public class Playlist {
 		return encontrado;
 	}
 	
+	/**
+	 * @deprecated
+	 * @param nombreCancion
+	 * @return si la cancion ha sido o no encontrada
+	 */
 	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
 	// encontrarCancionPorNombre(String nombre)
 	//Existe desde la version 1.0
