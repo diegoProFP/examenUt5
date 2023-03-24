f3adcd9d39661fa06ad2e396e5825987f571d27d
juanan30 <alumno@localhost>
2023-03-24 10:06:07 +0100

    se elimina la carpeta

--- Spotify/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"spotify.base"},{"l":"spotify.diegodi1"},{"l":"spotify.exception"},{"l":"spotify.juan.aparicio5"}];updateSearchResults();
