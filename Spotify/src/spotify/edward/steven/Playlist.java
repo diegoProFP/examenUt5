46f5fb474aa81dfb400195bedd34783cba193a3a
edwardCastroDAM <stevencastrobeltran@gmail.com>
2023-03-24 09:40:20 +0100

    clase copiada playlist

---
+++ Spotify/src/spotify/edward/steven/Playlist.java
@@ -0,0 +1,143 @@
+package spotify.edward.steven;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import spotify.base.Cancion;
+import spotify.exception.DatosInvalidosException;
+
+public class Playlist {
+	private String nombre;
+	private int totalSegundos;
+	private List<Cancion> canciones;
+
+	
+	// Poner descripcion, valor de retorno.
+	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
+	public double obtenerDuracionTotal() {
+		return (double) totalSegundos / 60;
+	}
+		
+		
+		
+	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
+	// referenciar tanto a las clases Cancion y DatosInvalidosException
+	//Existe desde la version 1.0
+	public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
+
+		boolean exito = false;
+		if (canciones == null) {
+			this.canciones = new ArrayList<>();
+		}
+		if (nueva == null || nueva.getNombre() == null) {
+			throw new DatosInvalidosException("La cancion o su nombre es vacía");
+		}
+
+		boolean existeCancion = this.encontrarCancionPorNombre(nueva.getNombre());
+		if (!existeCancion) {
+			exito = canciones.add(nueva);
+		}
+
+		return exito;
+	}
+
+
+	
+	// Poner descripcion, parametros de entrada, valor de retorno, y excepción.
+	//Existe desde la version 1.5
+	public boolean encontrarCancionPorNombre(String nombre) {
+		boolean encontrado = false;
+		int contador = 0;
+		while (!encontrado && contador < canciones.size()) {
+
+			// Si el nombre de la cancion obtenida en cada vuelta es igual a la que se
+			// quiere
+			if (canciones.get(contador).getNombre().equalsIgnoreCase(nombre)) {
+				encontrado = true;
+			}
+
+			contador++;
+		}
+
+		return encontrado;
+	}
+	
+	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
+	// encontrarCancionPorNombre(String nombre)
+	//Existe desde la version 1.0
+	public boolean encontrarCancion(String nombreCancion) {
+		boolean encontrado = false;
+
+		for (int cont = 0; cont < canciones.size(); cont++) {
+			Cancion cancionExtraida = this.canciones.get(cont);
+			if (cancionExtraida.getNombre().equals(nombreCancion)) {
+				encontrado = true;
+				break;
+			}
+			
+		}
+		return encontrado;
+	}
+
+	
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTotalSegundos() {
+		return totalSegundos;
+	}
+
+	public void setTotalSegundos(int totalSegundos) {
+		this.totalSegundos = totalSegundos;
+	}
+
+	public List<Cancion> getCanciones() {
+		return canciones;
+	}
+
+	public void setCanciones(List<Cancion> canciones) {
+		this.canciones = canciones;
+	}
+
+	//////////////////////  METODOS PRIVADOS ////////////////////////////
+
+
+//	private void comprobarErroresEliminar(String nombreCancion)
+//			throws DatosInvalidosException, OperacionNoPermitidaException {
+//		if (nombreCancion == null || nombreCancion.isEmpty()) {
+//			throw new DatosInvalidosException("La cancion o su nombre es vacía");
+//		}
+//
+//		if (canciones == null) {
+//			throw new OperacionNoPermitidaException(
+//					"No es posible eliminar la cancion. La lista de canciones está vacía");
+//		}
+//	}
+//	
+
+//	// Poner parametros de entrada, valor de retorno, y excepción. Además que pueda
+//		// referenciar a la clase DatosInvalidosException
+//	//Existe desde la version 1.0
+//	public boolean eliminarCancion(String nombreCancion) throws DatosInvalidosException, OperacionNoPermitidaException {
+//		boolean exito = false;
+//
+//		comprobarErroresEliminar(nombreCancion);
+//		
+//		boolean existeCancion = this.encontrarCancionPorNombre(nombreCancion);
+//		if (existeCancion) {
+//			Cancion eliminar = new Cancion();
+//			eliminar.setNombre(nombreCancion);
+//			exito = this.canciones.remove(eliminar);
+//		}
+//
+//		return exito;
+//	}
+	
+}
